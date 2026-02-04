5c4f349dfeeb274ba2d48409dddf7d52a50ea009
DEEPIKA P <1nt23cs057.deepika@nmit.ac.in>
2026-02-04 18:48:36 +0530

    Update app.js

--- my-node-app/app.js
+++ my-node-app/app.js
@@ -5,7 +5,8 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 // Setup multer for file uploads
 const storage = multer.diskStorage({
